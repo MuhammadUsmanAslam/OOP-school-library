5d3cef8120f4736eda942085b88fc004fa7dfcba
MuhammadUsmanAslam <musmanaslam520@gmail.com>
2023-05-24 14:06:17 +0500

    Fix rubocop errors

--- person.rb
+++ person.rb
@@ -1,7 +1,7 @@
 require_relative './nameable'
 require_relative './decorator'
-require_relative './capitalize-decorator'
-require_relative './trimmer-decorator'
+require_relative './capitalize_decorator'
+require_relative './trimmer_decorator'
 
 class Person < Nameable
   def initialize(age, name: 'Unknown', parent_permission: true)
@@ -42,8 +42,8 @@ end
 person = Person.new(22, name: 'maximilianus')
 puts person.correct_name
 
-capitalizedPerson = CapitalizeDecorator.new(person)
-puts capitalizedPerson.correct_name
+capitalized_person = CapitalizeDecorator.new(person)
+puts capitalized_person.correct_name
 
-capitalizedTrimmedPerson = TrimmerDecorator.new(capitalizedPerson)
-puts capitalizedTrimmedPerson.correct_name
+capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
+puts capitalized_trimmed_person.correct_name
